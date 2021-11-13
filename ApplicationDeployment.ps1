240dce3e343cbfa4dd148b5fd67a727530aa7e92
Gagan Rajpal <gagan2509rajpal@gmail.com>
2021-11-13 12:59:09 +0530

    Adding solutions

--- ApplicationDeployment.ps1
+++
@@ -1,6 +0,0 @@
-New-AzResourceGroup -Name ExampleGroup -Location "Central US"
-
-New-AzResourceGroupDeployment `
-  -Name ExampleDeployment `
-  -ResourceGroupName ExampleGroup `
-  -TemplateFile "\Problem1\ParentTemplate\MainTemplate.json"
